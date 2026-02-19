a48c9260a4236bd9c692544e998c5e5a357799c0
Mai Chi <nguyenmaichi0817@gmail.com>
2026-02-19 16:36:16 +0700

    Cập nhật sql và danh sách bài hát

--- database/web_music.sql
+++ database/web_music.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 08, 2026 at 05:15 PM
+-- Generation Time: Feb 19, 2026 at 10:34 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -34,6 +34,19 @@ CREATE TABLE `albums` (
   `release_year` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `albums`
+--
+
+INSERT INTO `albums` (`id`, `title`, `artist_id`, `release_year`) VALUES
+(1, 'COUP D`ETAT', 1, 2013),
+(2, 'Lover', 2, 2019),
+(3, 'm-tp M-TP', 3, 2017),
+(4, 'BẬT NÓ LÊN', 4, 2024),
+(5, 'Bảo tàng của nuối tiếc', 5, 2024),
+(6, '99%', 6, 2023),
+(7, 'ái', 7, 2023);
+
 -- --------------------------------------------------------
 
 --
@@ -45,6 +58,19 @@ CREATE TABLE `artists` (
   `name` varchar(200) NOT NULL,
   `country` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `artists`
+--
+
+INSERT INTO `artists` (`id`, `name`, `country`) VALUES
+(1, 'G-Dragon', 'Hàn Quốc'),
+(2, 'Taylor Swift', 'Hoa Kỳ'),
+(3, 'Sơn Tùng MTP', 'Việt Nam'),
+(4, 'SOOBIN', 'Việt Nam'),
+(5, 'VŨ', 'Việt Nam'),
+(6, 'RPT MCK', 'Việt Nam'),
+(7, 'tlinh', 'Việt Nam');
 
 -- --------------------------------------------------------
 
@@ -86,6 +112,26 @@ CREATE TABLE `songs` (
   `image_path` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Dumping data for table `songs`
+--
+
+INSERT INTO `songs` (`id`, `title`, `artist_id`, `album_id`, `duration`, `release_year`, `file_path`, `image_path`) VALUES
+(1, 'người điên', 7, 7, 227, 2023, 'music/nguoi_dien.mp3', 'images/nguoi_dien.jpg'),
+(2, 'nữ siêu anh  hùng', 7, 7, 331, 2023, 'music/nu_sieu_anh_hung.mp3', 'images/nu_sieu_anh_hung.jpg'),
+(3, 'Anh đã ổn hơn', 6, 6, 314, 2023, 'music/anh_da_on_hon.mp3', 'images/anh_da_on_hon.jpg'),
+(4, 'Cuốn Cho Anh Một Điếu Nữa Đi', 6, 6, 305, 2023, 'music/cuon_cho_anh_mot_dieu_nua_di.mp3', 'images/cuon_cho_anh_mot_dieu_nua_di.jpg'),
+(5, 'Mây Khóc Vì Điều Gì', 5, 5, 334, 2024, 'music/may_khoc_vi_dieu_gi.mp3', 'images/may_khoc_vi_dieu_gi.jpg'),
+(6, 'Nếu Những Tiếc Nuối', 5, 5, 420, 2024, 'music/neu_nhung_tiec_nuoi.mp3', 'images/neu_nhung_tiec_nuoi.jpg'),
+(7, 'Sunset In The City - Deluxe Version', 4, 4, 345, 2024, 'music/sunset_in_the_city.mp3', 'images/sunset_in_the_city.jpg'),
+(8, 'DANCING IN THE DARK', 4, 4, 347, 2024, 'music/dancing_in_the_dark.mp3', 'images/dancing_in_the_dark.jpg'),
+(9, 'Buông Đôi Tay Nhau Ra', 3, 3, 347, 2017, 'music/buong_doi_tay_nhau_ra.mp3', 'images/buong_doi_tay_nhau_ra.jpg'),
+(10, 'Âm Thầm Bên Em', 3, 3, 453, 2017, 'music/am_tham_ben_em.mp3', 'images/am_tham_ben_em.jpg'),
+(11, 'Cornelia Street', 2, 2, 447, 2019, 'music/cornelia_street.mp3', 'images/cornelia_street.jpg'),
+(12, 'Daylight', 2, 2, 453, 2019, 'music/daylight.mp3', 'images/daylight.jpg'),
+(13, 'CROOKED', 1, 1, 344, 2013, 'music/crooked.mp3', 'images/crooked.jpg'),
+(14, 'BLACK', 1, 1, 323, 2013, 'music/black.mp3', 'images/black.jpg');
+
 -- --------------------------------------------------------
 
 --
@@ -99,6 +145,14 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL,
   `role` enum('user','admin') DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `role`) VALUES
+(1, 'admin01', 'admin01@gmail.com', '$2a$10$abcxyzHashGia', 'admin'),
+(2, 'user01', 'user01@gmail.com', '$2a$10$abcxyzHashGia', 'user');
 
 --
 -- Indexes for dumped tables
@@ -155,13 +209,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `albums`
 --
 ALTER TABLE `albums`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `artists`
 --
 ALTER TABLE `artists`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `playlists`
@@ -173,13 +227,13 @@ ALTER TABLE `playlists`
 -- AUTO_INCREMENT for table `songs`
 --
 ALTER TABLE `songs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
