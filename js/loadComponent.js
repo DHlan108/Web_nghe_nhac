5a0d76d69446622cc1a2bd72a7c0abd8f96ea3ab
Mai Chi <nguyenmaichi0817@gmail.com>
2026-03-19 11:48:21 +0700

    Cap nhat

--- js/loadComponent.js
+++ js/loadComponent.js
@@ -22,10 +22,30 @@ const currentPage = window.location.pathname.split("/").pop() || "home.html";
                 }
             });
         });
-loadSidebar();
+
 
 fetch("../component/player.html")
   .then(res => res.text())
   .then(data => {
     document.getElementById("player-container").innerHTML = data;
   });
+
+document.addEventListener("click", (e) => {
+  if (e.target.closest(".logout")) {
+    const ok = confirm("Đăng xuất khỏi tài khoản này?");
+    if (!ok) return;
+
+    fetch("../api/logout.php", {
+      method: "POST"
+    })
+    .then(res => res.json())
+    .then(data => {
+      if (data.success) {
+        window.location.href = "login.html";
+      }
+    })
+    .catch(() => {
+      alert("Lỗi đăng xuất!");
+    });
+  }
+});
