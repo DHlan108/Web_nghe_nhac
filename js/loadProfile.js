43beb1a20c3fbf1faa7579fd4e7a042e16353585
DHlan108 <danghoanglan2005@gmail.com>
2026-03-19 21:11:40 +0700

    lưu du lieu ca nhan

--- js/loadProfile.js
+++ js/loadProfile.js
@@ -1,20 +1,14 @@
-fetch("../api/profile.php", {
-    credentials: "include"
+fetch("/Web_nghe_nhac/api/profile.php", {
+  credentials: "include",
 })
-.then(res => res.json())
-.then(data => {
-
-    if(data.success){
-
-        document.getElementById("username").innerText = data.user.username;
-        document.getElementById("email").innerText = data.user.email;
-        document.getElementById("role").innerText = data.user.role;
-
-    }else{
-
-        alert("Bạn chưa đăng nhập!");
-        window.location.href = "login.html";
-
+  .then((res) => res.json())
+  .then((data) => {
+    if (data.success) {
+      document.getElementById("username").innerText = data.user.username;
+      document.getElementById("email").innerText = data.user.email;
+      document.getElementById("role").innerText = data.user.role;
+    } else {
+      alert("Bạn chưa đăng nhập!");
+      window.location.href = "login.html";
     }
-
-});
+  });
